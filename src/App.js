3ff4c7543415a14e4078eb13d4e0e2a228406959
MHYeganeh <mhyeganeh73@gmail.com>
2024-09-19 19:49:54 +0330

    Add search functionality

--- src/App.js
+++ src/App.js
@@ -6,27 +6,43 @@ class App extends Component {
         super();
 
         this.state = {
+            searchQuery: '',
             monsters: [],
         }
     }
 
     componentDidMount() {
         this.getData();
-    }
+    };
 
-    getData() {
+    getData = () => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then((users) => {
                 this.setState({monsters: users})
             })
             .catch(err => console.log(err));
-    }
+    };
+
+    onSearchChange = (event) => {
+        this.setState({searchQuery: event.target.value})
+    };
 
     render() {
+        const filtered_monsters = this.state.monsters.filter((monster) => {
+            return monster.name.includes(this.state.searchQuery)
+        });
+
         return (
             <div className='App'>
-                {this.state.monsters.map((monster, index) => {
+
+                <input type="search"
+                       className='search-box'
+                       placeholder='search monsters...'
+                       onChange={this.onSearchChange}
+                />
+
+                {filtered_monsters.map((monster, index) => {
                     return <h1 key={monster.id}>{index+1}- {monster.name}</h1>
                 })}
             </div>
