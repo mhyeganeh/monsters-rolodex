4207bc8a575d09ea5cee62e5482d8913f409cb4e
MHYeganeh <mhyeganeh73@gmail.com>
2024-09-19 18:38:41 +0330

    use loops for static data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Component } from "react";
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,26 +6,21 @@ class App extends Component {
         super();
 
         this.state = {
-            name: {
-                firstName: "M.Hasan",
-                lastName: "Yeganeh",
-            },
-            country: "IRAN",
-        };
+            monsters: [
+                {id: 1, name: "Linda"},
+                {id: 2, name: "Frank"},
+                {id: 3, name: "Jacky"},
+                {id: 4, name: "Andrei"},
+            ],
+        }
     }
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>{this.state.name.firstName} {this.state.name.lastName},  from <b>"{this.state.country}"</b></p>
-                    <button onClick={() => {
-                        this.setState({name: {firstName: "John", lastName: "Doe"}, country: "USA"});
-                    }}>
-                        Change Name!
-                    </button>
-                </header>
+            <div className='App'>
+                {this.state.monsters.map(monster => {
+                    return <h1 key={monster.id}>{monster.name}</h1>
+                })}
             </div>
         );
     }
