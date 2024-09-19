0076291fd88c9fb23387e08e182e3b8e411df3c0
MHYeganeh <mhyeganeh73@gmail.com>
2024-09-19 17:52:19 +0330

    Add constructor & local state to component

--- src/App.js
+++ src/App.js
@@ -3,22 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            name: "Ali",
+        };
+    }
+
     render() {
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        In the name of Allah
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
+                    <p>Hello {this.state.name}</p>
+                    <button>Change Name!</button>
                 </header>
             </div>
         );
